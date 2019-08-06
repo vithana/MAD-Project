98da07be569bbb48f5ff3d98d6f8b49d68fcec91
NandanaX <50739907+NandanaX@users.noreply.github.com>
2019-08-07 02:14:18 +0530

    Created food view page

--- FoodHut/app/src/main/java/com/example/foodhut/OfferFragment.java
+++ FoodHut/app/src/main/java/com/example/foodhut/OfferFragment.java
@@ -15,5 +15,6 @@ public class OfferFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_offer, container, false);
+
     }
 }
