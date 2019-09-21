0d95ccf28ea6fbe2110dceb1321b684194684c35
blcchanaka <blcchanaka@gmail.com>
2019-09-22 01:06:27 +0530

    Done modifications

--- FoodHut/app/src/main/java/com/example/foodhut/UserOfferDetails.java
+++ FoodHut/app/src/main/java/com/example/foodhut/UserOfferDetails.java
@@ -2,13 +2,16 @@ package com.example.foodhut;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.bumptech.glide.Glide;
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.example.foodhut.database.Offer;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
@@ -16,76 +19,50 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class UserOfferDetails extends AppCompatActivity {
-
 
     TextView offer_name,offer_price,offer_description;
     ImageView offer_img;
     CollapsingToolbarLayout collapsingToolbarLayout;
     FloatingActionButton btnCart;
     ElegantNumberButton numberButton;
-
     String offerId="";
-
-    FirebaseDatabase database;
+    StorageReference storageReference;
     DatabaseReference offers;
+
+    private ProgressDialog dialog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_offer_details);
 
-        database = FirebaseDatabase.getInstance();
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Please Wait");
+
+        storageReference = FirebaseStorage.getInstance().getReference("offers");
         offers = FirebaseDatabase.getInstance().getReference("offers");
 
         numberButton = findViewById(R.id.number_button);
         btnCart = findViewById(R.id.crtBtn);
-
         offer_description = findViewById(R.id.offer_description);
         offer_name = findViewById(R.id.offer_name);
         offer_price = findViewById(R.id.offer_price);
         offer_img = findViewById(R.id.img_offer);
-
-
         collapsingToolbarLayout = findViewById(R.id.collapsing);
-
 
         collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppbar);
         collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppbar);
 
-
         offerId = getIntent().getStringExtra("offer_name");
+        dialog.show();
         getDetailsOffer(offerId);
-
-
-
-
     }
 
     private void getDetailsOffer(String offerId) {
-
-//        offers.child(offerId).addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                Offer offer = dataSnapshot.getValue(Offer.class);
-//
-//
-//                Picasso.get().load(offer.getImageId()).into(offer_img);
-//
-//                collapsingToolbarLayout.setTitle(offer.getOfferName());
-//                offer_price.setText((int) offer.getPrice());
-//                offer_name.setText(offer.getOfferName());
-//                offer_description.setText(offer.getDescription());
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
 
         offers.child(offerId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -97,6 +74,20 @@ public class UserOfferDetails extends AppCompatActivity {
                 offer_price.setText(temp);
                 offer_name.setText(offer.getOfferName());
                 offer_description.setText(offer.getDescription());
+
+                storageReference.child(offer.getImageId()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // Got the download URL for 'users/me/profile.png'
+                        Glide.with(UserOfferDetails.this).load(uri.toString()).centerCrop().into(offer_img);
+                        dialog.dismiss();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle any errors
+                    }
+                });
             }
 
             @Override
@@ -104,6 +95,5 @@ public class UserOfferDetails extends AppCompatActivity {
 
             }
         });
-
     }
 }
--- FoodHut/app/src/main/java/com/example/foodhut/UserProductDetails.java
+++ FoodHut/app/src/main/java/com/example/foodhut/UserProductDetails.java
@@ -3,12 +3,16 @@ package com.example.foodhut;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.bumptech.glide.Glide;
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.example.foodhut.database.Product;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
@@ -16,56 +20,49 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class UserProductDetails extends AppCompatActivity {
-
-
 
     TextView product_name,product_price;
     ImageView product_img;
     CollapsingToolbarLayout collapsingToolbarLayout;
     FloatingActionButton btnCart;
     ElegantNumberButton numberButton;
-
     String productId="";
-
-    FirebaseDatabase database;
+    StorageReference storageReference;
     DatabaseReference products;
+
+    private ProgressDialog dialog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_product_details);
 
-        database = FirebaseDatabase.getInstance();
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Please Wait");
+
+        storageReference = FirebaseStorage.getInstance().getReference("products");
         products = FirebaseDatabase.getInstance().getReference("products");
 
         numberButton = findViewById(R.id.number_button);
         btnCart = findViewById(R.id.crtBtn);
-
-
         product_name = findViewById(R.id.product_name);
         product_price = findViewById(R.id.product_price);
         product_img = findViewById(R.id.img_product);
-
-
         collapsingToolbarLayout = findViewById(R.id.collapsing);
-
 
         collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppbar);
         collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppbar);
 
-
         productId = getIntent().getStringExtra("product_name");
+        dialog.show();
         getDetailsProduct(productId);
-
-
-
-
     }
 
     private void getDetailsProduct(String productId) {
-
 
         products.child(productId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -78,8 +75,19 @@ public class UserProductDetails extends AppCompatActivity {
                 product_price.setText(temp);
                 product_name.setText(product.getFoodName());
 
-
-
+                storageReference.child(product.getImageId()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // Got the download URL for 'users/me/profile.png'
+                        Glide.with(UserProductDetails.this).load(uri.toString()).centerCrop().into(product_img);
+                        dialog.dismiss();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle any errors
+                    }
+                });
             }
 
             @Override
